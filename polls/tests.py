f30e8fec479046677de6c4a4adcfcce43ddf54ce
RudiWolff <158566646+RudiWolff@users.noreply.github.com>
2025-03-30 17:59:18 +0200

    Cambiado bucles for sequenciales a bucles for anidados en tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -45,10 +45,13 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.assertEqual( self.selenium.title , "Site administration | Django site admin" )
         
         # Definició de preguntes per inserir
-        test_questions = ["What's ya name?", "What's ya pet's name?"]
-
+        matrix = {
+            "What's ya name?": ["Joan", "Ariadna"], 
+            "What's ya pet's name?": ["Fify", "Kitty"]
+            }
+        
         # introduïm dades per pregunta
-        for question in test_questions:
+        for question in matrix:
             # Buscar al butón 'Add' de 'Questions'
             self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()
             
@@ -64,50 +67,24 @@ class MySeleniumTests(StaticLiveServerTestCase):
             # comprovem de nou que el títol de la pàgina és el què esperem
             self.assertEqual( self.selenium.title , "Select question to change | Django site admin" )
         
-        # introduïm dades per a 'Choice'
-        # matrix = {"What's ya name?": ["Joan", "Ariadna"], "What's ya pet's name?": ["Fify", "Kitty"]}
-        q_name = ["What's ya name?"]
-        name_choices = ["Joan", "Ariadna"]
-        q_pet = ["What's ya pet's name?"]
-        pet_choices = ["Fify", "Kitty"]
-
-        for name in name_choices:
-            # accedir a la pàgina d'afegeix opcions des de el menú lateral
-            self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/choice/add/']").click()
- 
-            # self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/choice/add'))
-            # comprovem que el títol de la pàgina és el què esperem
-            self.assertEqual( self.selenium.title , "Add choice | Django site admin" )
-            
-            # Rellenar el formulari
-            # Seleccionem la cuestió correcta
-            select_element = self.selenium.find_element(By.NAME, "question")
-            select = Select(select_element)
-            select.select_by_visible_text("What's ya name?")
-            
-            choice_text = self.selenium.find_element(By.NAME, "choice_text") 
-            choice_text.send_keys(name)
-            self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
-            
-            # comprovem de nou que el títol de la pàgina és el què esperem
-            self.assertEqual( self.selenium.title , "Select choice to change | Django site admin" )
-
-        for name in pet_choices:
-        # accedir a la pàgina d'afegeix preguntes
-        
-            self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/choice/add'))
-            # comprovem que el títol de la pàgina és el què esperem
-            self.assertEqual( self.selenium.title , "Add choice | Django site admin" )
-            
-            # Rellenar el formulari
-            # Seleccionem la cuestió correcta
-            select_element = self.selenium.find_element(By.NAME, "question")
-            select = Select(select_element)
-            select.select_by_visible_text("What's ya pet's name?")
-            
-            choice_text = self.selenium.find_element(By.NAME, "choice_text") 
-            choice_text.send_keys(name)
-            self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
-            
-            # comprovem de nou que el títol de la pàgina és el què esperem    
-            self.assertEqual( self.selenium.title , "Select choice to change | Django site admin" )
+            # introduïm dades per a 'Choice'
+            for option in matrix[question]:
+                # accedir a la pàgina d'afegeix opcions des de el menú lateral
+                self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/choice/add/']").click()
+    
+                # self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/choice/add'))
+                # comprovem que el títol de la pàgina és el què esperem
+                self.assertEqual( self.selenium.title , "Add choice | Django site admin" )
+                
+                # Rellenar el formulari
+                # Seleccionem la cuestió correcta
+                select_element = self.selenium.find_element(By.NAME, "question")
+                select = Select(select_element)
+                select.select_by_visible_text(question)
+                
+                choice_text = self.selenium.find_element(By.NAME, "choice_text") 
+                choice_text.send_keys(option)
+                self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+                
+                # comprovem de nou que el títol de la pàgina és el què esperem
+                self.assertEqual( self.selenium.title , "Select choice to change | Django site admin" )
