0194de11a470b7b6ced9e29d6544367dca27bd50
RudiWolff <rudi.wolff@yahoo.com>
2025-03-31 13:30:16 +0200

    Comprobación del test aún incompleta modificat:        polls/tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -55,36 +55,48 @@ class MySeleniumTests(StaticLiveServerTestCase):
             # Buscar al butón 'Add' de 'Questions'
             self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()
             
+            # comprovem de nou que el títol de la pàgina és el què esperem
+            self.assertEqual( self.selenium.title , "Add question | Django site admin" )
+            
             # Rellenar el formulari
             question_text = self.selenium.find_element(By.NAME, "question_text") 
             question_text.send_keys(question)
             
+            # Expandir menú "Date Information" y rellenar campos
+            self.selenium.find_element(By.ID, "fieldset-0-1-heading").click()
+
             question_date = self.selenium.find_element(By.LINK_TEXT,"Today").click()
             question_time = self.selenium.find_element(By.LINK_TEXT,"Now").click()
             
+            index = 0
+            
+            # introduïm dades per a 'Choice'
+            for option in matrix[question]:
+                # Introducir valor para opcion de index X
+                choice_text = self.selenium.find_element(By.NAME, f"choice_set-{index}-choice_text") 
+                choice_text.send_keys(option)
+                
+                # Aumentar index para la siguiente opción
+                index += 1
+            
+            # Guardar el set de 1 pregunta con 2 opciones
             self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
             
             # comprovem de nou que el títol de la pàgina és el què esperem
             self.assertEqual( self.selenium.title , "Select question to change | Django site admin" )
-        
-            # introduïm dades per a 'Choice'
+            
+        # Comprobación de la inserción correcta
+        # Wieder in For Schleife
+        for question in matrix:
+            self.selenium.find_element(By.LINK_TEXT, question).click()
+
+            # Búsqueda de las opciones
             for option in matrix[question]:
-                # accedir a la pàgina d'afegeix opcions des de el menú lateral
-                self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/choice/add/']").click()
-    
-                # self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/choice/add'))
-                # comprovem que el títol de la pàgina és el què esperem
-                self.assertEqual( self.selenium.title , "Add choice | Django site admin" )
-                
-                # Rellenar el formulari
-                # Seleccionem la cuestió correcta
-                select_element = self.selenium.find_element(By.NAME, "question")
-                select = Select(select_element)
-                select.select_by_visible_text(question)
-                
-                choice_text = self.selenium.find_element(By.NAME, "choice_text") 
-                choice_text.send_keys(option)
-                self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
-                
-                # comprovem de nou que el títol de la pàgina és el què esperem
-                self.assertEqual( self.selenium.title , "Select choice to change | Django site admin" )
+                if self.selenium.find_element(By.XPATH, f'//input[@value="{option}"]'):
+                    print(f"Opción {option} correcta")
+
+                # By.LINK_TEXT matrix[erste Fragen] .click()
+                # find_element input[@value=matrix[question]] 
+            
+            # Volver a la lista de las preguntas
+            self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/question/'))
