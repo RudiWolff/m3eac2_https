7a16ef2775fb5bc4d89b29bd2849f19f66c52d04
RudiWolff <rudi.wolff@yahoo.com>
2025-03-31 11:50:59 +0200

    Incorporado el apartado "Adding related objects" de "Writing your first Django app, part 7" 	modificat:        polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-from .models import Choice
 
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
 
